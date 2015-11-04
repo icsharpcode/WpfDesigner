ddba849db97a7baeff46e0f62a51f059997b10fe
jogibear9988 <jochen.kuehner@gmx.de>
2015-11-04 14:11:34 +0100

    Button to Zoom to 100%

--- WpfDesign.Designer/Project/Controls/ZoomButtons.cs
+++ WpfDesign.Designer/Project/Controls/ZoomButtons.cs
@@ -19,6 +19,8 @@
 using System;
 using System.Windows;
 using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using ICSharpCode.WpfDesign.UIExtensions;
 
 namespace ICSharpCode.WpfDesign.Designer.Controls
 {
@@ -37,13 +39,16 @@ namespace ICSharpCode.WpfDesign.Designer.Controls
 			var uxPlus = (ButtonBase)Template.FindName("uxPlus", this);
 			var uxMinus = (ButtonBase)Template.FindName("uxMinus", this);
 			var uxReset = (ButtonBase)Template.FindName("uxReset", this);
-			
+			var ux100Percent = (ButtonBase)Template.FindName("ux100Percent", this);
+
 			if (uxPlus != null)
 				uxPlus.Click += OnZoomInClick;
 			if (uxMinus != null)
 				uxMinus.Click += OnZoomOutClick;
 			if (uxReset != null)
 				uxReset.Click += OnResetClick;
+			if (ux100Percent != null)
+				ux100Percent.Click += On100PercentClick;
 		}
 		
 		const double ZoomFactor = 1.1;
@@ -61,6 +66,25 @@ namespace ICSharpCode.WpfDesign.Designer.Controls
 		void OnResetClick(object sender, EventArgs e)
 		{
 			SetCurrentValue(ValueProperty, 1.0);
+		}
+		void On100PercentClick(object sender, EventArgs e)
+		{
+			var zctl = this.TryFindParent<ZoomControl>();
+			var contentWidth = ((FrameworkElement) zctl.Content).ActualWidth;
+			var contentHeight = ((FrameworkElement)zctl.Content).ActualHeight;
+			var width = zctl.ActualWidth;
+			var height = zctl.Height;
+
+			if (contentWidth > width || contentHeight > height)
+			{
+				double widthProportion = contentWidth / width;
+				double heightProportion = contentHeight / height;
+
+				if (widthProportion > heightProportion)
+					SetCurrentValue(ValueProperty, (width - 20.00) / contentWidth);
+				else
+					SetCurrentValue(ValueProperty, (height - 20.00) / contentHeight);
+			}			
 		}
 	}
 }
