c5448e4d1d8bf4d931c83f28feb0f9794acabe29
jkuehner <jochen.kuehner@kardex.com>
2015-12-22 08:52:06 +0100

    Include a SimpleSample wich uses the Nuget

---
+++ SimpleSample/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml;
+using ICSharpCode.WpfDesign.Designer.Services;
+using ICSharpCode.WpfDesign.Designer.Xaml;
+using ICSharpCode.WpfDesign.XamlDom;
+
+namespace SimpleSample
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private static string xaml = @"<Grid 
+xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+xmlns:d=""http://schemas.microsoft.com/expression/blend/2008""
+xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006""
+mc:Ignorable=""d""
+x:Name=""rootElement"" Background=""White""></Grid>";
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			using (var xmlReader = XmlReader.Create(new StringReader(xaml)))
+			{
+				designSurface.LoadDesigner(xmlReader, new XamlLoadSettings());
+			}
+		}
+		
+		private void lstControls_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var item = lstControls.SelectedItem as ToolBoxItem;
+			if (item != null)
+			{
+				var tool = new CreateComponentTool(item.Type);
+				designSurface.DesignPanel.Context.Services.Tool.CurrentTool = tool;		  
+			}
+		}
+
+		private void Export()
+		{
+			var sb = new StringBuilder();
+			using (var xmlWriter = new XamlXmlWriter(sb))
+			{
+				designSurface.SaveDesigner(xmlWriter);
+			}
+			var xamlCode = sb.ToString();
+		}
+	}
+}
---
+++ SimpleSample/ToolboxItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SimpleSample
+{
+	public class ToolBoxItem
+	{
+		public Type Type { get; set; }
+
+		private object _instance;
+		public object Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = Activator.CreateInstance(Type);
+				}
+				return _instance;
+			}
+		}
+		
+		public string Name
+		{
+			get
+			{
+				return Type.Name;
+			}
+		}
+	}
+}
