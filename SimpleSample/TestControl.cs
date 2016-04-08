6f9314227ffb216dfc3da7f3305439fbbd61aa6a
jkuehner <jochen.kuehner@kardex.com>
2016-04-08 22:12:41 +0200

    Test Control in Simple Sample

---
+++ SimpleSample/TestControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SimpleSample
+{
+	public class TestControl : Control
+	{
+		public Color TestColor
+		{
+			get { return (Color)GetValue(TestColorProperty); }
+			set { SetValue(TestColorProperty, value); }
+		}
+
+		public static readonly DependencyProperty TestColorProperty =
+			DependencyProperty.Register("TestColor", typeof(Color), typeof(TestControl), new PropertyMetadata());
+	}
+}
