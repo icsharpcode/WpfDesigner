c0ffbd35913b3fc31cb9485a26896ba1fcb20b26
jkuehner <jochen.kuehner@kardex.com>
2018-07-01 17:44:38 +0200

    feature : Collection Editor -> better display of some types

---
+++ WpfDesign.Designer/Project/PropertyGrid/Editors/CollectionTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ICSharpCode.WpfDesign.Designer.PropertyGrid.Editors
+{
+	public class CollectionTemplateSelector : DataTemplateSelector
+	{
+		public override DataTemplate SelectTemplate(object item, DependencyObject container)
+		{
+			FrameworkElement element = container as FrameworkElement;
+			if (element != null && item != null && item is DesignItem)
+			{
+				var di = item as DesignItem;
+				if (di.Component is Point)
+				{
+					return element.FindResource("PointTemplate") as DataTemplate;
+				}
+				else if (di.Component is String)
+				{
+					return element.FindResource("StringTemplate") as DataTemplate;
+				}
+				return element.FindResource("DefaultTemplate") as DataTemplate;
+			}
+
+			return null;
+		}
+	}
+}
