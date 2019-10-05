354a5b5302f9045f608cd793e3d5d7f3ad998fe1
Peter Amrehn <peter.wendorff@gmx.de>
2019-10-05 12:05:10 +0200

    Implement failing test: Test fails by exception
    This test should be extended later to check the correct output, but first step would be just not to crash.

---
+++ WpfDesign.Designer/Tests/Designer/TransformTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using ICSharpCode.WpfDesign.Designer;
+using NUnit.Framework;
+
+namespace ICSharpCode.WpfDesign.Tests.Designer
+{
+	public class TransformTests : ModelTestHelper
+	{
+		internal static void Rotate(double angle, DesignItem item)
+		{
+			ModelTools.ApplyTransform(item, new RotateTransform(angle, 0.5, 0.5));
+		}
+
+		[Test]
+		public void RotateSkewedButton()
+		{
+			DesignItem button = CreateCanvasContext(
+				"<Button Content='Button' Width='75' Height='23' RenderTransformOrigin='0.5,0.5'>"
+				+ "<Button.RenderTransform>"
+				+ "<SkewTransform AngleY='-45' />"
+				+ "</Button.RenderTransform>"
+				+ "</Button>");
+			Rotate(30, button);
+			//AssertCanvasDesignerOutput();
+		}
+	}
+}
